a3a4f70e29ade71b9aec40f9acb512f469d71f7b
pbiuser07 <168774218+pbiuser07@users.noreply.github.com>
2025-05-02 19:35:54 +0200

    changes

--- dags/dbt/ai_adoption/models/intermediate/inter_roi.py
+++ dags/dbt/ai_adoption/models/intermediate/inter_roi.py
@@ -12,8 +12,8 @@ def model(dbt, session):
    df_pandas = df.to_pandas()
 
    #creating a copy with reduced columns
-   df_pandas_reduced = df_pandas.reindex(['country_code','year','company','ai_investment_usd_mm','return_from_ai_usd_mm','industry_code'],axis=1).copy()
-
+   df_pandas_reduced = df_pandas.copy()
+#reindex(['country_code','year','company','ai_investment_usd_mm','return_from_ai_usd_mm','industry_code'],axis=1).
    #convert column headings to lower case for better handling
    df_pandas_reduced.columns = map(str.lower,df_pandas_reduced.columns)
 
